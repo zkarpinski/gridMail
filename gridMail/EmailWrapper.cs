bfa8222ab68297ab1bc34873dc1cbbfef3b103a0
Zachary Karpinski <zachary.karpinski@nationalgrid.com>
2016-08-19 11:17:17 -0400

    initial commit
    init

---
+++ gridMail/EmailWrapper.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Exchange.WebServices.Data;
+
+
+namespace gridMail {
+    public class EmailWrapper {
+        // Public properties
+        public string body { get; set; }
+        public string subject { get; set; }
+        public string sender { get; set; }
+
+
+        // Declare the variables
+        private bool use_html = false;
+        private string template_file;
+        private string html_body;
+        private List<string> attachments;
+        private List<Tuple<string, string>> inline_attachments;
+        private List<string> recipients;
+        private List<string> distribution_groups;
+        private ExchangeService exchange_service;
+        private const string exchange_uri = "https://url.com/ews/exchange.asmx";
+
+
+
+        // Constructor
+        public EmailWrapper() {
+            this.attachments = new List<string>();
+            this.inline_attachments = new List<Tuple<string, string>>();
+            this.recipients = new List<string>();
+            this.distribution_groups = new List<string>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Integer 1: Success, -1 = Fail</returns>
+        public int Send() {
+            // Connect to exchange_service
+            exchange_service = new ExchangeService(ExchangeVersion.Exchange2010);
+            exchange_service.Url = new Uri(exchange_uri);
+            exchange_service.UseDefaultCredentials = true;
+
+            // Create the email
+            EmailMessage emailMessage = new EmailMessage(exchange_service);
+
+            // Subject
+            emailMessage.Subject = this.subject;
+
+            //Body
+            if (use_html) {
+                emailMessage.Body = new MessageBody(BodyType.HTML, this.html_body);
+
+            }
+            else { 
+                emailMessage.Body = new MessageBody(BodyType.HTML, this.body);
+            }
+
+            // Add Recipients
+            foreach (string rep in this.recipients) {
+                emailMessage.ToRecipients.Add(rep);
+            }
+            // Add Distribution List Persons
+            foreach (string dist in this.distribution_groups) {
+                ExpandGroupResults grp = exchange_service.ExpandGroup(dist);
+                foreach (EmailAddress em in grp.Members) {
+                    emailMessage.ToRecipients.Add(em.Address);
+                }
+            }
+
+
+            //Add Attachements
+            foreach (string att in this.attachments) {
+                emailMessage.Attachments.AddFileAttachment(att);
+            }
+
+            // Add Inline attachments
+            //https://msdn.microsoft.com/en-us/library/office/hh532564(v=exchg.80).aspx
+            foreach (Tuple<string,string> tup in this.inline_attachments) {
+                string contentID = tup.Item1;
+                string file = tup.Item2;
+
+                FileAttachment fileAttachment = emailMessage.Attachments.AddFileAttachment(file);
+                fileAttachment.ContentId = contentID;
+                fileAttachment.IsInline = true;
+            }
+
+
+            // Save and send
+            try {
+                emailMessage.Save();
+                emailMessage.SendAndSaveCopy();   
+            }
+            catch {
+                return -1;
+            }
+            return 0;
+        }
+
+        public void AddAttachment(string file) {
+            //Verify file exists
+            if (File.Exists(file)) {
+                this.attachments.Add(file);
+            }
+            else {
+                // Give error message
+                Console.WriteLine("{0} the file does not exist.\nPress Enter to exit...", file);
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+
+        public void AddInlineAttachment(string s) {
+            // Split the text.
+            string cID, file;
+            cID = "<" + s.Split(';')[0] + ">";
+            file = s.Split(';')[1];
+            Tuple<string, string> tup = new Tuple<string, string>(cID, file);
+
+            //Verify file exists
+            if (File.Exists(file)) {
+                this.inline_attachments.Add(tup);
+            }
+            else {
+                // Give error message
+                Console.WriteLine("{0} the file does not exist.\nPress Enter to exit...", file);
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+
+        public void AddRecipient(string address) {
+            this.recipients.Add(address);
+        }
+
+        public void AddGroup(string grp) {
+            this.distribution_groups.Add(grp);
+        }
+
+        public void UseHTML(string file) {
+            //Verify file exists
+            if (File.Exists(file)) {
+                this.template_file = file;
+                this.html_body = File.ReadAllText(file);
+                this.use_html = true;
+            }
+            else {
+                // Give error message
+                Console.WriteLine("{0} the file does not exist.\nPress Enter to exit...", file);
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
---
+++ gridMail/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gridMail {
+    class Program {
+        static void Main(string[] args) {
+
+            // Instantiate the email wrapper
+            EmailWrapper new_email = new EmailWrapper();
+
+            // Parse the args and pass the email wrapper
+            ParseArgs(args, ref new_email);
+
+            // Send Email
+            int result = new_email.Send();
+
+            Environment.Exit(result);
+        }
+
+        private static void PrintHelpMenu() {
+            Console.WriteLine("\nusage: gridMail.exe [-a -b -f -g -h -i -s -t -w]");
+            Console.WriteLine("\t-a --attachment [item1,item2]");
+            Console.WriteLine("\t-b --body [Email body text]");
+            Console.WriteLine("\t-g --group [group@domain.com,group2@domain.com]");
+            Console.WriteLine("\t-i --inplace [contentID;file,contentId;file2]");
+            Console.WriteLine("\t-f --from [sender@domain.com]");
+            Console.WriteLine("\t-t --to [person@domain.com,person2@domain.com]");
+            Console.WriteLine("\t-w --web [path_to_html_file]   Use HTML template");
+            Console.WriteLine(@"example: gridMail.exe -a ""item1.txt,item2.txt"" -t ""person1@domain.com,person2@email.com"" -b ""Body goes here"" -s ""Subject Line"" -f sender@email.com");
+            Console.WriteLine("\nPress enter to exit...");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        private static void ParseArgs(string[] args, ref EmailWrapper email) {
+            //Read each argument
+            int args_count = args.Length;
+
+            if (args_count <= 0) { PrintHelpMenu(); }
+            for (int i = 0; i < args_count; i++) {
+
+                switch (args[i]) {
+                    case "-h":
+                    case "--help":
+                        PrintHelpMenu();
+                        break;
+                    case "-a": //Attachments
+                    case "--attachment":
+                        // Split the next argument by semicolons and commas
+                        if (i + 1 < args_count) {
+                            foreach (string att in args[i + 1].Split(new Char[] { ',', ';' })) {
+                                email.AddAttachment(att);
+                            }
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the attachments list [-a]");
+                            PrintHelpMenu();
+                        }
+                        break;
+                    case "-b": // Email body
+                    case "--body":
+                        if (i + 1 < args_count) {
+                            email.body = args[i + 1];
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the body argument [-b]");
+                            PrintHelpMenu();
+                        }
+                        break;
+                    case "-g": // Distribution Groups
+                    case "--group":
+                        // Split the next argument by semicolons and commas
+                        if (i + 1 < args_count) {
+                            foreach (string grp in args[i + 1].Split(new Char[] { ',', ';' })) {
+                                email.AddGroup(grp);
+                            }
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the distrubution group list [-g]");
+                            PrintHelpMenu();
+                        }
+                        break;
+                    case "-i": //Inline Attachments
+                    case "--inline":
+                        // Split the next argument by semicolons and commas
+                        if (i + 1 < args_count) {
+                            foreach (string s in args[i + 1].Split(',')) {
+                                email.AddInlineAttachment(s);
+                            }
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the attachments list [-a]");
+                            PrintHelpMenu();
+                        }
+                        //Console.WriteLine("Inline attachments not yet supported");
+                        i++;
+                        break;
+                    case "-t": // Recipients
+                    case "--to":
+                        if (i + 1 < args_count) {
+                            foreach (string t in args[i + 1].Split(new Char[] { ',', ';' })) {
+                                email.AddRecipient(t);
+                            }
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the recipients list [-t]");
+                            PrintHelpMenu();
+                        }
+                        break;
+                    case "-s": //Subject Line
+                    case "--subject":
+                        if (i + 1 < args_count) {
+                            email.subject = args[i + 1];
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the subject argument [-s]");
+                            PrintHelpMenu();
+                        }
+                        break;
+                    case "-f": // Sender
+                    case "--from":
+                        if (i + 1 < args_count) {
+                            email.sender = args[i + 1];
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the from argument [-f]");
+                            PrintHelpMenu();
+                        }
+                        break;
+                    case "-w": // Use HTML Template
+                    case "--web":
+                        if (i + 1 < args_count) {
+                            email.UseHTML(args[i + 1]);
+                            i++;
+                        }
+                        else {
+                            Console.WriteLine("No argument proceeding the from argument [-w]");
+                            PrintHelpMenu();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invaild input: {0}", args[i]);
+                        PrintHelpMenu();
+                        break;
+                }
+            }
+        }
+    }
+}
